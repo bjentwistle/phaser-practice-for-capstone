e3b8e2d8463ee4c22c457e16a74f5b07e84215f3
Becky <bjentwistle@gmail.com>
2023-07-09 17:11:22 +0100

    managed to get sprites to work

--- Scene2.js
+++ Scene2.js
@@ -6,41 +6,83 @@ class Scene2 extends Phaser.Scene {
     }
     
     create() {
-        this.config = this.sys.game.config;
+        this.config = this.sys.game.config; //makes sure that config is available in this scene
         //this.background = this.add.image(0, 0, "background");
-        //this.background = this.add.tileSprite(0, 0, this.config.width, this.config.height, "background");
-        //this.background.setOrigin(0, 0);
+        this.background = this.add.tileSprite(0, 0, this.config.width, this.config.height, "background");
+        this.background.setOrigin(0, 0);
   
-        this.ship1 = this.add.image(this.config.width / 2 - 150, this.config.height / 2, "ship");
-        this.ship2 = this.add.image(this.config.width / 2, this.config.height / 2, "ship2");
-        this.ship3 = this.add.image(this.config.width / 2 + 150, this.config.height / 2, "ship3");
+        this.ship1 = this.add.sprite(this.config.width / 2 - 150, this.config.height / 2, "ship");
+        this.ship2 = this.add.sprite(this.config.width / 2, this.config.height / 2, "ship2");
+        this.ship3 = this.add.sprite(this.config.width / 2 + 150, this.config.height / 2, "ship3");
         this.ship1.setScale(2);
-        this.ship1.setOrigin(0.5);
-        this.ship1.flipY = true;
-        this.ship1.angle = 90;
+        // this.ship1.setOrigin(0.5);
+        // this.ship1.flipY = true;
+        // this.ship1.angle = 90;
 
         this.ship3.setScale(2);
-        this.ship3.setOrigin(0.5);
-        this.ship3.flipY = true;
-        this.ship3.angle = 270;
+        // this.ship3.setOrigin(0.5);
+        // this.ship3.flipY = true;
+        // this.ship3.angle = 270;
 
         this.ship2.setScale(2);
-        this.ship2.setOrigin(0.5);
+        // this.ship2.setOrigin(0.5);
+
+         //create animations
+         this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+         });
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+          });
+
+        //play the animations
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+        //make the ships clickable to destroy them
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        // object destroyed
+        this.input.on('gameobjectdown', this.destroyShip, this);
 
         this.add.text(20, 20, "Playing game!", {
           font: "30px Arial",
           fill: "black"
         });
+
     }
 
     update(){
         // to call a function to move the ships vertically
         this.moveShip(this.ship1, 1);
-        this.moveShip(this.ship2, 2);
-        this.moveShip(this.ship3, 3);
+        this.moveShip(this.ship2, 1.5);
+        this.moveShip(this.ship3, 2);
 
     // to scroll the background image
-    //this.background.tilePositionY -= 0.5;
+    this.background.tilePositionY -= 0.5;
 
     }
 
@@ -64,6 +106,14 @@ class Scene2 extends Phaser.Scene {
     const randomX = Phaser.Math.Between(0, this.config.width);
     ship.x = randomX;
   }
+
+  // destroy ships function
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
+  }
+
+  
 
 }//END BRACKET 
 
