7333695ba059e0571b4161b55d695bba02008094
Becky <bjentwistle@gmail.com>
2023-07-10 15:43:24 +0100

    added window width and height

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
-// window.onload = function(){
 
-//     const game = new Phaser.Game(config);
-// }
-//NOT NEEDED ?
+window.onload = function(){
+    this.config = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: 0xB61731,
+        scene: [Scene1, Scene2],
+        pixelArt: true,
+        fps: {
+            target: 60,
+            forceSetTimeOut: true
+        }
+      }
+      const game = new Phaser.Game(this.config);
+}
